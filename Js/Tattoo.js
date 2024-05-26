ce1d0ffc275abf3c04f80d819fd92aca89f7b282
Sumit Sohal <sumitsohal2018@gmail.com>
2024-05-26 10:38:38 -0400

    corrected book now button

--- Js/Tattoo.js
+++ Js/Tattoo.js
@@ -4,58 +4,41 @@ bookButton.addEventListener('click', function() {
   window.location.href = "contact.html";
 });
 
-    const navToggle = document.querySelector('.nav-toggle');
-    const nav = document.querySelector('.nav');
+const navToggle = document.querySelector('.nav-toggle');
+const nav = document.querySelector('.nav');
 
-    navToggle.addEventListener('click', () => {
-        nav.classList.toggle('nav-open');
-    });
+navToggle.addEventListener('click', () => {
+  nav.classList.toggle('nav-open');
+});
 
-
-
-// // Get the form element and add a submit event listener
+// Uncomment and use this part if needed in the future
 // const form = document.querySelector('form');
 // form.addEventListener('submit', function(event) {
-//   // Prevent the default form submission behavior
 //   event.preventDefault();
-
-//   // Get the form data
 //   const formData = new FormData(form);
-
-//   // Send the form data to the server using AJAX
 //   const xhr = new XMLHttpRequest();
 //   xhr.open('POST', '/submit-your-inquiry');
 //   xhr.send(formData);
-
-//   // Show a success message to the user
 //   alert('Your inquiry has been submitted successfully!');
-
-//   // Clear the form fields
 //   form.reset();
 // });
-// const newsletterForm = document.querySelector('.newsletter form');
-// const emailInput = document.querySelector('.newsletter input[type="email"]');
-// const submitButton = document.querySelector('.newsletter button[type="submit"]');
 
-// newsletterForm.addEventListener('submit', (event) => {
-//   event.preventDefault();
+const newsletterForm = document.querySelector('.newsletter form');
+const emailInput = document.querySelector('.newsletter input[type="email"]');
+const submitButton = document.querySelector('.newsletter button[type="submit"]');
 
-  // Check if the email input is empty
-  // if (emailInput.value === '') {
-  //   alert('Please enter your email address.');
-  //   return;
-  // }
-
-  // Check if the email input is valid
-  // const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
-  // if (!emailRegex.test(emailInput.value)) {
-  //   alert('Please enter a valid email address.');
-  //   return;
-  // }
-
+newsletterForm.addEventListener('submit', (event) => {
+  event.preventDefault();
+  if (emailInput.value === '') {
+    alert('Please enter your email address.');
+    return;
+  }
+  const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+  if (!emailRegex.test(emailInput.value)) {
+    alert('Please enter a valid email address.');
+    return;
+  }
   // Send the form data to the server
   // You can use fetch() or XMLHttpRequest() to send the data
-
-  // Disable the submit button to prevent multiple submissions
-//   submitButton.disabled = true;
-// });
+  submitButton.disabled = true;
+});
